6d57435e6209fbc11d732612064e38e10c50152b
gudlad <19a31@sdmit.in>
2023-03-12 11:42:47 +0530

    added files

---
+++ ReverseStringPreserveSpace.java
@@ -0,0 +1,34 @@
+public class ReverseStringPreserveSpace {
+
+    public static char[] reservePreserveSpace(char[] source, char[] destination) {
+        int j = destination.length - 1;
+
+        // Transfer spaces from source to destination array
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] == ' ') {
+                destination[i] = source[i];
+            }
+        }
+
+        // Transfer non-space characters in reverse order
+        for (int i = 0; i < source.length; i++) {
+            if (source[i] != ' ' && destination[j] != ' ') {
+                destination[j] = source[i];
+                j--;
+            }
+        }
+
+        return destination;
+    }
+
+    public static void main(String[] args) {
+        String str = "Hello World";
+        char[] strArray = str.toCharArray();
+        char[] duplStr = new char[strArray.length];
+
+        // Reverse the string while preserving the position of spaces
+        String reverse = new String(reservePreserveSpace(strArray, duplStr));
+
+        System.out.println(reverse);
+    }
+}
