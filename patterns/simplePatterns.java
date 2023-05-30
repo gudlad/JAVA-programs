15d11a90ff54da0fa70bee1abe6b9a143983bd82
gudlad <19a31@sdmit.in>
2023-05-30 22:30:46 +0530

    patterns

---
+++ patterns/simplePatterns.java
@@ -0,0 +1,62 @@
+// package patterns;
+
+public class simplePatterns{
+
+    // public static void main(String[] args)
+    // {
+    //     for(int i=0;i<5;i++)
+    //     {
+    //         for(int j=0;j<=i;j++)
+    //         {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    
+    // public static void main(String[] args)
+    // {
+    //     for(int i=0;i<5;i++)
+    //     {
+    //         for(int j=5;j>i;j--)
+    //         {
+    //             System.out.print("*");
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+
+    // public static void main(String[] args)
+    // {
+        
+    //     for(int i=0;i<5;i++)
+    //     {
+    //         int num=1;
+    //         for(int j=0;j<=i;j++)
+    //         {
+    //             System.out.print(num++);
+    //         }
+    //         System.out.println();
+    //     }
+    // }
+
+    
+    public static void main(String[] args)
+    {
+        
+        for(int i=0;i<=10;i++)
+        {
+           for(int j=0;;j++)
+           {
+
+           }
+           for(int k=0;k<=10;k++)
+           {
+            
+           }
+        }
+    }
+
+}
